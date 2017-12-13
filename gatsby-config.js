439ee79e5427f47395cd5d14aa09f7387ac192cf
Alex Nelson <w.alexnelson@gmail.com>
2017-12-13 02:00:35 -0700

    add meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  plugins: [],
+  siteMetadata: {
+    title: 'walex',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
 };
---
+++ src/layouts/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+
+import '../assets/styles/global.css';
+import favicon from '../assets/images/icon.png';
+
+const TemplateWrapper = ({ children }) => (
+  <div className="application">
+    <Helmet>
+      <title>walex</title>
+      <link rel="icon" type="image/png" href={ favicon } />
+    </Helmet>
+    {children()}
+  </div>
+);
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func.isRequired,
+};
+
+export default TemplateWrapper;
