b33524886570f1f5d8ae783c5bdc0338bbf06a26
Alex Nelson <w.alexnelson@gmail.com>
2026-05-31 19:42:57 -0600

    chore: remove gatsby project files

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LinkIcon } from './LinkIcon';
-export { default as SocialLinks } from './SocialLinks';
-export { default as Unsplash } from './Unsplash';
-export { default as Header } from './Header';
